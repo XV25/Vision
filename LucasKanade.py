a5cdfa4e12155548cf077f3ea0293d2cb961e0b3
XV25 <erwann.landais@ensta-bretagne.org>
2020-02-17 17:07:14 +0100

    New Kanade

--- LucasKanade.py
+++ LucasKanade.py
@@ -14,57 +14,98 @@ import matplotlib.pylab as plt
 
 plt.close('all')
 
-fname  = "Data/Trans/image1.png" # 0-255 / 0-15 / 181-230
-img_C  = cv2.imread(fname)     # Lecture image en couleurs BGR
+def get_from_pic(img1, img2, mask, color2):
+    
+    #name1 = "Data/Trans/image1.png" # 0-255 / 0-15 / 181-230
+    #img_C  = cv2.imread(name1)     # Lecture image en couleurs BGR
+    
+    img_C = img1
+    
+    img_C = cv2.resize(img_C,(640,480))
+    
+    old_gray = cv2.cvtColor(img_C, cv2.COLOR_BGR2GRAY)
+    
+    
+    color = np.random.randint(0,255,(100,3))
 
-img_C = cv2.resize(img_C,(640,480))
+    L = detect_color_vid(img_C, color2 )
+    
+    M = np.zeros((1,1,2), np.float32)
+    
+    M[0,:,:] = np.array(L)
+    
+    
+    feature_params = dict( maxCorners = 100,
+                           qualityLevel = 0.3,
+                           minDistance = 7,
+                           blockSize = 7 )
+    
+    # p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    
+    p0 = M
+    
+    #fname  = "Data/Trans/image2.png" # 0-255 / 0-15 / 181-230
+    #img_C  = cv2.imread(name2)     # Lecture image en couleurs BGR
+    
+    img_C = img2
+    
+    img_C = cv2.resize(img_C,(640,480))
+    
+    frame_gray = cv2.cvtColor(img_C, cv2.COLOR_BGR2GRAY)
+    
+    
+    lk_params = dict( winSize  = (120,120),
+                      maxLevel = 2,
+                      criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.01))
 
-old_gray = cv2.cvtColor(img_C, cv2.COLOR_BGR2GRAY)
-
-mask = np.zeros_like(img_C)
-
-color = np.random.randint(0,255,(100,3))
-
-L = detect_color_vid(img_C,'B')
-
-M = np.zeros((1,1,2), np.float32)
-
-M[0,:,:] = np.array(L)
-
-
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
-
-# p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-
-p0 = M
-
-fname  = "Data/Trans/image2.png" # 0-255 / 0-15 / 181-230
-img_C  = cv2.imread(fname)     # Lecture image en couleurs BGR
-
-img_C = cv2.resize(img_C,(640,480))
-
-frame_gray = cv2.cvtColor(img_C, cv2.COLOR_BGR2GRAY)
-
-
-lk_params = dict( winSize  = (100,100),
-                  maxLevel = 10,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 0.01))
-
-
-# calculate optical flow
-p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-
-good_new = p1[st==1]
-good_old = p0[st==1]
-
+    
+    # calculate optical flow
+    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    
+    good_new = p1[st==1]
+    good_old = p0[st==1]
+    
+    
+    
     # draw the tracks
-for i,(new,old) in enumerate(zip(good_new,good_old)):
-    a,b = new.ravel()
-    c,d = old.ravel()
-    mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-    frame = cv2.circle(img_C,(a,b),5,color[i].tolist(),-1)
-img = cv2.add(img_C,mask)
-cv2.imshow('frame',img)
+    for i,(new,old) in enumerate(zip(good_new,good_old)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+        frame = cv2.circle(img_C,(a,b),5,color[i].tolist(),-1)
+        
+    return(img_C,mask,good_new,good_old)
+    
+
+def get_from_vid(name_vid):
+    vidcap = cv2.VideoCapture(name_vid)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output2.avi', fourcc, 20.0, (640,  480))
+    k = 0
+    color = 'J'
+    success,old_image = vidcap.read()
+    mask = np.zeros_like(old_image)
+    while success:
+        success,new_image = vidcap.read()
+        if success:
+            imgC,mask,gn,go = get_from_pic(old_image, new_image, mask ,color)
+            new_image = cv2.add(imgC,mask)
+            out.write(new_image)
+            cv2.imshow('frame %i'%k,new_image)
+            k+=1
+            old_image = new_image
+        if (k > 120):
+            success = False
+    out.release()
+    cv2.destroyAllWindows()
+    return(gn,go)
+
+if __name__ == "__main__":
+    img1  = cv2.imread("Data/Trans/image1.png")     # Lecture image en couleurs BGR
+    img2 = cv2.imread("Data/Trans/image2.png")     # Lecture image en couleurs BGR
+    
+    #get_from_pic(img1,img2)
+    gn,go = get_from_vid('output.avi')
+    
+    
+    
