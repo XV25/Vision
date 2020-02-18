4f7b679da97706ae2dceab15cb1f1316c3c80a02
nathanfourniol <nathan.fourniol@ensta-bretagne.org>
2020-02-18 13:11:35 +0100

    pb detection 1er point

--- lucas_kanade_nathan.py
+++ lucas_kanade_nathan.py
@@ -18,7 +18,8 @@ lk_params = dict( winSize  = (15,15),
 color = np.random.randint(0,255,(100,3))
 
 
-cap = cv2.VideoCapture('nathan.avi')
+#cap = cv2.VideoCapture('nathan.avi')
+cap = cv2.VideoCapture(0)
 # Take first frame and find corners in it
 ret, old_frame = cap.read()
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
@@ -26,29 +27,42 @@ old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 
 hsv = cv2.cvtColor(old_frame, cv2.COLOR_BGR2HSV)
     
-lower_blue = np.array([0,90,110])
-upper_blue = np.array([24,255,255])
+lower_blue = np.array([164,0,100])
+upper_blue = np.array([198,255,177])
 
 # Threshold the HSV image to get only blue colors
 mask = cv2.inRange(hsv, lower_blue, upper_blue)
+cv2.imshow('res',mask)      
+kernelf = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+kernelo = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelf)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernelo)
 # Bitwise-AND mask and original image
 #res = cv2.bitwise_and(frame,frame, mask= mask)
-
+res = old_frame.copy()
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #[1:]
-T = 0
-L = []
 cx,cy = 0,0
+
+Mmax = 0
+cnt_max = None
+
 for i in range (len(contours)):
     cnt = contours[i]
     M = cv2.moments(cnt)
-    if M['m00'] > 400:
-        cx = int(M['m10']/(M['m00']+1*10**-5))
-        cy = int(M['m01']/(M['m00']+1*10**-5))
-        #cv2.circle(res,(cx,cy), 4, (0,255,100), -1) 
-        break
-    
+    if M['m00'] > Mmax:
+        Mmax = M['m00']
+        cnt_max = cnt
+ 
+M = cv2.moments(cnt_max)       
+cx = int(M['m10']/(M['m00']+1*10**-5))
+cy = int(M['m01']/(M['m00']+1*10**-5))
+cv2.circle(mask,(cx,cy), 4, (0,255,100), -1) 
+        
+
+cv2.imshow('res2',mask)      
 M = np.zeros((1,1,2), np.float32)
 M[0,:,:] = np.array([cx,cy])
+#M[0,:,:] = np.array([300,300])
 p0 = M
 
 
@@ -62,11 +76,11 @@ while(1):
 
     # calculate optical flow
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-
+    
     # Select good points
     good_new = p1[st==1]
     good_old = p0[st==1]
-
+    
     # draw the tracks
     for i,(new,old) in enumerate(zip(good_new,good_old)):
         a,b = new.ravel()
